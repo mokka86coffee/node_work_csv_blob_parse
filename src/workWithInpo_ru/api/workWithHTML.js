d812bdbfbca06f85fb04c007d158a1892fad2db5
mokka86coffee <mokka86coffee@gmail.com>
2019-04-03 09:51:27 +0300

    commit

--- src/workWithInpo_ru/api/workWithHTML.js
+++ src/workWithInpo_ru/api/workWithHTML.js
@@ -4,12 +4,12 @@ const { priceCalculation, reduceItem, workingWithName } = require('./transformHt
 
 module.exports = function (html, catalogTitle, addToIdx, amirogen, idTitle) {
     
-    let data = 'META.CSV::CATEGORY_ID_EXTERNAL|CATALOG_ID_PARENT_EXTERNAL;CATEGORY_ID_PARENT_EXTERNAL;CATEGORY_DESCRIPTION;CATEGORY_ANNOUNCE;CATEGORY_FULL_DESCRIPTION;CATEGORY_IS_DELETED;CATALOG_IS_DELETED;CATALOG_ID_EXTERNAL;CATALOG_DESCRIPTION;CATALOG_NAME_FULL;CATALOG_MAIN_PRICE;IMAGE_IMAGE_MAIN;IMAGE_IMAGE_SMALL;IMAGE_IMAGE_POPUP;CATALOG_CODE;CATALOG_HTML_TITLE;CATALOG_HTML_KEYWORDS;CATALOG_HTML_DESCRIPTION;CATALOG_HTML_IS_AUTOGEN;CATALOG_CUSTOM_FIELD_12;CATALOG_CUSTOM_FIELD_25\n';
-
+    
     let tableRows = parserHtml(html).querySelectorAll('table.b_items_list tbody tr');
 
+    amirogen += ';' + catalogTitle + ';;;false;false';
 
-    tableRows.forEach( (el,idx) => {
+    let data = tableRows.reduce( (res, el, idx) => {
         let sku = parserHtml(el.innerHTML).querySelector('span[itemprop="sku"]').innerText;
         let price = priceCalculation( parserHtml(el.innerHTML).querySelector('td.bil_price').innerText );
         
@@ -18,10 +18,9 @@ module.exports = function (html, catalogTitle, addToIdx, amirogen, idTitle) {
         let imgFileName = idTitle + '_zzmain';
         let imgLink = 'rashodniki/metchiki_plashki/' + imgFileName + '.jpg';
 
-        amirogen += ';' + catalogTitle + ';;;false;false';
         
-        data += `${amirogen};${idTitle}${idx + addToIdx};${title};${htmlBody};${price};${imgLink};${imgLink};${imgLink};${sku};${seoTitle};${seoKeywords};${title};false;Китай;На складе\n`;    
-    });
+        return res + `${amirogen};${idTitle}${idx + addToIdx};${title};${htmlBody};${price};${imgLink};${imgLink};${imgLink};${sku};${seoTitle};${seoKeywords};${title};false;Китай;На складе\n`;    
+    }, '');
 
     console.log('Всего элементов - ', tableRows.length);
     return data;
