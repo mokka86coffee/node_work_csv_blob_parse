ef47c7d42fec35a4a57d5ceb6c08443b90bce33e
mokka86coffee <mokka86coffee@gmail.com>
2019-04-05 08:50:11 +0300

    commit

--- src/workWithInpo_ru/mainFuncs.js
+++ src/workWithInpo_ru/mainFuncs.js
@@ -8,9 +8,9 @@ const writeToCSVFile = require('./api/writeToFile');
 
 (async() => {
 
-let URL = `http://www.inpo.ru/shop/S:${592}`,
+let URL = `http://www.inpo.ru/shop/S:${744}`,
     catalogTitle = 'Оправки и втулки',
-    addToIdx = 120, 
+    addToIdx = 350, 
     amirogen = 'Amiro_gen_90493;Amiro_gen_90360'
 ;
 
