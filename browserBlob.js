f22043bda4cc53aff96bf3aba867dab672d08d46
mokka86coffee <mokka86coffee@gmail.com>
2019-04-09 11:19:39 +0300

    commit

--- browserBlob.js
+++ browserBlob.js
@@ -1,1 +1,12 @@
- let getFileFromBlob = (data, config = { type: 'json' }) => { let blob = new Blob([JSON.stringify(data, null, '\t')], config); let file = URL.createObjectURL(blob); let link = document.createElement('a'); link.href = file; link.download = 'data.json'; link.click(); }; let data = Array.from(document.querySelectorAll('.product__name a')).map( el => el.href ).filter( (_,idx) => idx !== 0); getFileFromBlob(data);
+let getFileFromBlob = (data, config = { type: 'json' }) => {
+	let blob = new Blob([JSON.stringify(data, null, '\t')], config);
+	let file = URL.createObjectURL(blob);
+	let link = document.createElement('a');
+	link.href = file;
+	link.download = 'data.json';
+	link.click();
+};
+let data = Array.from(document.querySelectorAll('.product__name a'))
+	.map(el => el.href)
+	.filter((_, idx) => idx !== 0);
+getFileFromBlob(data);
