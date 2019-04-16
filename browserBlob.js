2d533007e8cf5d611ff528b3b79ffbaa18cba7f3
mokka86coffee <mokka86coffee@gmail.com>
2019-04-16 10:29:42 +0300

    promise added

--- browserBlob.js
+++ browserBlob.js
@@ -24,21 +24,30 @@ function getUrl(part) {
 (async() => {
 
 	let iframe = document.createElement('iframe');
+	iframe.style.display = 'none';
+	document.body.appendChild(iframe);
 		
-	try {
+	await new Promise( resolve => {
 
-		while (true) {
-			
-			iframe.src = 'https://stanok74.ru/katalog/internet-magazin/dlja-listovogo-metalla/gilotinnye-nozhnicy/gidravlicheskie-gilotiny?action=rsrtme&catid=20088&offset=124';
-			iframe.onload = () => console.log(iframe.contentWindow);
-			
-			// console.log(iframe.contentWindow.document.querySelector('.eshop-item-list__container'));
-			throw new Error('aaaaaa');
+		try {
+
+			while (true) {
+				
+				iframe.src = 'https://stanok74.ru/katalog/internet-magazin/dlja-listovogo-metalla/gilotinnye-nozhnicy/gidravlicheskie-gilotiny?action=rsrtme&catid=20088&offset=1211';
+				iframe.onload = () => console.log(iframe.contentWindow.document);
+				
+				// console.log(iframe.contentWindow.document.querySelector('.eshop-item-list__container'));
+				throw new Error('aaaaaa');
+			}
+	
+		} catch(err) {
+			console.log(err);
+			resolve();
 		}
 
-	} catch(err) {
-		console.log(err);
-	}
+	});
+
+	document.body.removeChild(iframe);
 
 })();
 
