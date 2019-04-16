7cc49785a5c86a38a149a4ab82b8e9c46687eb60
mokka86coffee <mokka86coffee@gmail.com>
2019-04-16 12:25:38 +0300

    done

--- browserBlob.js
+++ browserBlob.js
@@ -30,14 +30,14 @@ function getUrl(part) {
 	const container = document.querySelector('.eshop-item-list__container');
 	const pager = document.querySelector('.pager');
 	const btnShowAll = document.createElement('button');
-	btnShowAll.className = 'btn';
+	btnShowAll.className = 'btn btn-info';
 	btnShowAll.innerText = 'Показать все';
 	pager.appendChild(btnShowAll);
 
 	const href = window.location.href;
 		
 	for( let offset = 12; ; offset += 12 ) {
-		pager.innerHTML = '<p>Загрузка...</p>';
+		// pager.innerHTML = '<p>Загрузка...</p>';
 
 		try {
 			await new Promise ( (resolve,reject) => {
@@ -58,13 +58,122 @@ function getUrl(part) {
 		}
 		
 		catch(err) {
-			console.log(err);
-			pager.style.display = 'none';
+			// pager.style.display = 'none';
 			break;
 		}
 	}
 
-	document.body.removeChild(iframe);
+	// document.body.removeChild(iframe);
 
 })();
 
+jQuery(document).ready(function($){
+
+	let catMenuLinks = document.querySelectorAll('.menu_catAll--first_lvl a');
+	let  menuSmCatIcon_LinkFollow = function(e) {
+		 if (e.target.href) { location.assign(e.target.href) }
+	}
+	catMenuLinks.forEach(el=>el.addEventListener('click', menuSmCatIcon_LinkFollow) );
+
+  //-----------------Видос на главной
+	$('.block3-sliderr').hover(
+		function(){ 
+			 $(this).addClass('video-vstavka'); 
+		}, 
+		function(){ 
+			 $(this).addClass('video-vstavka'); 
+		});
+  //-----------------Видос на главной
+
+  //-----------------Перемещение поиска
+	
+  if ( document.documentElement.clientWidth < 816 ) { 
+	let search = document.querySelector('[name="small_search"]');
+	let lay_f1 = document.querySelector('#lay_f2');
+	lay_f1.append(search);
+  }
+
+  //-----------------Перемещение поиска
+
+  //-----------------Раскрытие меню
+
+  if ( document.documentElement.clientWidth < 816 ) { 
+	let menu_catAll = document.querySelector('.menu_catAll');
+	let mobileMenu = document.querySelector('.mobile-menu');
+
+	menu_catAll.addEventListener('click',(e)=>{
+		 menu_catAll.classList.toggle('mobile-catalog--opened');
+		 mobileMenu.classList.remove('mobile-menu--opened'); 
+		 return false;
+	});
+
+	mobileMenu.addEventListener('click',(e)=>{
+		 mobileMenu.classList.toggle('mobile-menu--opened'); 
+		 menu_catAll.classList.remove('mobile-catalog--opened');
+		 return false;
+	});
+  }
+
+  //-----------------Раскрытие меню
+
+
+
+//    <!-- Скрытие email -->
+	  jQuery('.name_bttm:contains(info@stanok74.ru)').text('')
+		  .append('<div class="btn btn-info">Показать e-mail</div>')
+		  .on('click', function(){yaCounter21066178.reachGoal('SHOW_EMAIL_PRESSED');});
+	  jQuery('.name_bttm div.btn').on('click',function(){
+		  jQuery(this).hide().parent().text('info@stanok74.ru');
+		  
+	  });
+//    <!-- Скрытие email -->
+
+});
+
+{
+	(async() => {
+
+		let iframe = document.createElement('iframe');
+		iframe.style.display = 'none';
+		document.body.appendChild(iframe);
+	
+		const container = document.querySelector('.eshop-item-list__container');
+		const pager = document.querySelector('.pager');
+		const btnShowAll = document.createElement('button');
+		btnShowAll.className = 'btn btn-info';
+		btnShowAll.innerText = 'Показать все';
+		pager.appendChild(btnShowAll);
+	
+		const href = window.location.href;
+			
+		for( let offset = 12; ; offset += 12 ) {
+			// pager.innerHTML = '<p>Загрузка...</p>';
+	
+			try {
+				await new Promise ( (resolve,reject) => {
+			
+					iframe.src = `${href}&offset=${offset}`;
+					iframe.onload = () => {
+							
+						const items = iframe.contentWindow.document.querySelectorAll('.eshop-item-list__container > span');
+						
+						if (!items.length) reject( Error('no pages left') );
+	
+						items.forEach( span => container.appendChild(span) );
+						resolve();
+					
+					}
+	
+				});
+			}
+			
+			catch(err) {
+				// pager.style.display = 'none';
+				break;
+			}
+		}
+	
+		// document.body.removeChild(iframe);
+	
+	})();
+}
