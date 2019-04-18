c651f6eb8b8330c8e77e7fe1ee3038ddabbc4e81
mokka86coffee <mokka86coffee@gmail.com>
2019-04-18 13:19:11 +0300

    commit

--- src/workWithInpo_ru/api/transformHtmlToValues.js
+++ src/workWithInpo_ru/api/transformHtmlToValues.js
@@ -24,8 +24,8 @@ function reduceItem (param) {
 function workingWithName (title, catalogTitle) {
     // let kolvoZubiev = name.match(/Z{1}\s?={1}\s?\d{1,3}/gi)[0].replace(/Z{1}\s?={1}\s?/,'');
     // let material = name.match(/кл.т/gi)[0].replace(/Z{1}\s?={1}\s?\d{1,3}\s/,'');
-    let dimensions = title.match(/^[\d]{1,3}х?/gi);
-    console.log(diametr);
+    let dimensions = title.match(/\d{1,3}\s?х{1}/gi);
+    console.log(dimensions);
     // let dlina = name.match(/\d{1,3},?\d{1,3}\s?х/gi)[1].replace('х','');
     // let grad = name.match(/\d{1,3}(\s)?град/gi)[0].replace(/(\s)?град/gi,'');
     
