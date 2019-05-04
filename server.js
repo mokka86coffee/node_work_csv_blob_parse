293c34103450d73cbe6349853f9fe154ff043210
mokka86coffee <43421744+mokka86coffee@users.noreply.github.com>
2019-05-05 00:40:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const fs = require('fs')
 const path = require('path')
 
 http.createServer( (req, resp) => {
-    const url = req.url.substr(1)
-    const way = path.resolve(__dirname).substr(0, way.indexOf('/src')) + url
-    console.log(url)
+    let url = req.url.substr(1)
+    if ( /^dist/.test(url) ) url = 'web/' + url 
+
+    let way = path.resolve(__dirname)
+    way = way.substr(0, way.indexOf('/src')) + '/' + url
+
+    console.log('url - ', url)
+    console.log('way - ', way)
+    console.log()
     
     if ( url.includes('.html') ) {
         fs.readFile(url, (err, data) => {
@@ -14,6 +20,13 @@ http.createServer( (req, resp) => {
             resp.end()
         })
     } 
+    else if ( url.includes('.map') ) {
+        fs.readFile(way, (err, data) => {
+            resp.writeHead( 200, { 'Content-Type': 'application/json' } )
+            resp.write(data)
+            resp.end()
+        })
+    }
     else if ( url.includes('.css') ) {
         fs.readFile(way, (err, data) => {
             resp.writeHead( 200, { 'Content-Type': 'text/css' } )
@@ -28,5 +41,28 @@ http.createServer( (req, resp) => {
             resp.end()
         })
     } 
+    else if ( /\.ico/.test(url) ) {
+        fs.readFile('/home/evgen/Desktop/arendatika/web/images/favicons/favicon.ico', (err, data) => {
+            resp.writeHead( 200, { 'Content-Type': 'image/x-icon' } )
+            resp.write(data)
+            resp.end()
+        })
+    } 
+    else if ( url.includes('.js') ) {
+        fs.readFile(way, (err, data) => {
+            resp.writeHead( 200, { 'Content-Type': 'text/javascript' } )
+            resp.write(data)
+            resp.end()
+        })
+    }
+    else if ( /(\.woff|\.woff2)/.test(url) ) {
+        fs.readFile(way, (err, data) => {
+            resp.writeHead( 200, { 'Content-Type': 'pplication/font-woff' } )
+            resp.write(data)
+            resp.end()
+        })
+    } else {
+        resp.end()
+    }
 
 }).listen(2000)
