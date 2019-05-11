97facab56aee923850cf150da9de9393bbb4b98c
mokka86coffee <43421744+mokka86coffee@users.noreply.github.com>
2019-05-11 11:25:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 class ResponseTransform {
     constructor(resp, url){
@@ -26,6 +26,21 @@ class ResponseTransform {
         }
     }
 
+    get includePHPTypes (){
+        function read(filename) {
+            return fs.readFileSync(filename).toString()
+        }
+        
+        const way = this.way + 'src/Client/';
+
+        return {
+            'header': read(`${way}components/header.php`),
+            'footer': read(`${way}components/footer.php`),
+            'addon_head': read(`${way}html_templates/website/addon_head.php`),
+            'addon_footer': read(`${way}html_templates/website/addon_footer.php`)
+        };
+    }
+
     urlTransform (url){
         const { localway } = this
 
@@ -42,50 +57,42 @@ class ResponseTransform {
 
     transformPHPInclude(html) {
 
-        const { way } = this
+        const { includePHPTypes: types } = this;
 
-        const header = fs.readFileSync(`${way}/src/Client/components/header.php`).toString()
-        const footer = fs.readFileSync(`${way}/src/Client/components/footer.php`).toString()
-
-        html = html.replace(
-            /<\?php include\(.+header.+\); \?>/g,
-            header
-        )
+        for (const type in types) {
+            const regexp = new RegExp(`<\\?php+.+include\\(+.+${type}\\.php+.+\\?>`,'g');
+            html = html.replace(regexp, types[type]);
+        };
     
-        html = html.replace(
-            /<\?php include\(.+footer.+\); \?>/g,
-            footer
-        )
-    
-        return html
+        return html;
     }
 
     testRequest(){
 
-        const { url, mimeTypes } = this
+        const { url, mimeTypes } = this;
 
         const key = Object.keys( mimeTypes )
             .filter( type => RegExp( type.replace(/\./g,'\\.') ).test(url) )
-            .toString()
+            .toString();
         
         if ( key && key !== '.html' ) {
-            this.mimeType = mimeTypes[key]
-            this.url = url.replace(/\?.+/,'')
+            this.mimeType = mimeTypes[key];
+            this.url = url.replace(/\?.+/,'');
         }
 
         else if ( key === '.html' ) {
-            this.mimeType = mimeTypes[key]
-            ResponseTransform.prototype.localway = path.dirname(url)
-            this.url = 'src/Client/html_templates/' + url 
+            this.mimeType = mimeTypes[key];
+            ResponseTransform.prototype.localway = path.dirname(url);
+            this.url = 'src/Client/html_templates/' + url;
         } 
 
         else if ( /[\?\.]/.test(url) ) {
-            this.mimeType = 'unknown'
+            this.mimeType = 'unknown';
         }
         
         else {
-            this.url = 'src/Client/html_templates/' + url
-            this.mimeType = 'dir'
+            this.url = 'src/Client/html_templates/' + url;
+            this.mimeType = 'dir';
         }
 
     }
@@ -118,15 +125,20 @@ class ResponseTransform {
         } 
         else if ( mimeType === 'dir' ) {
 
-            this.resp.write(`<style>a{padding: 10px;color: #20576d;}</style>`)
+            this.resp.write(`<style>a{display: block; padding: 10px; color: #20576d; margin: 10px;}</style>`)
             
             const url = this.url.replace(/.+\//, '')
     
-            fs.readdirSync(way)
-                .filter( name => /\.html/.test(name) || !/\./.test(name) )
-                .forEach( name => 
-                    this.resp.write(`<a href="${url}/${name}">${name}</a>`)
-                )
+            fs.readdirSync(way, { withFileTypes: true })
+                .filter( ({ name }) => /\.html/.test(name) || !/\./.test(name) )
+                .sort( dirent => dirent.isDirectory() ? -1 : 1 )
+                .forEach( dirent =>{
+                    const { name } = dirent;
+                    this.resp.write( dirent.isDirectory()
+                        ? `<a style="background: #eee;" href="${url}/${name}">${name}</a>`
+                        : `<a href="${url}/${name}">${name}</a>`
+                    );        
+                });
 
             this.resp.end()
         } 
@@ -143,11 +155,11 @@ class ResponseTransform {
 http.createServer( (request, response) => {
     const url = request.url.substr(1)
 
-    const RespTransform = new ResponseTransform(response, url)
+    const RespTransform = new ResponseTransform(response, url);
 
-    RespTransform.testRequest()
+    RespTransform.testRequest();
     
-    RespTransform.writeResponse()
+    RespTransform.writeResponse();
 
 }).listen(2000)
 
